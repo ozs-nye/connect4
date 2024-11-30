5a3ec0d1af0db7d768e4f3b299746d9f2b221688
ozs-nye <olah.zsolt@student.nye.hu>
2024-11-30 15:53:56 +0100

    log-olás (logback) élesítése [info, error]

--- src/main/java/connect4/Main.java
+++ src/main/java/connect4/Main.java
@@ -2,11 +2,15 @@ package connect4;
 
 import connect4.Csomagok.*;
 import java.io.*;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 
 /**
  * Main.java A főprogram.
  */
 public class Main {
+  // Log-olás beállítása, meghívása
+  private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
   // Ezeket csak akkor vesszük figyelembe, ha nincs mentett állás, amit be kellene tölteni
   public static int tablaSorokSzama = 2;
@@ -25,6 +29,8 @@ public class Main {
    * @param args Megvizsgáljuk, hogy az alkalmazás indításakor a 'hs' (HighScore) paraméter meg lett-e adva.
    */
   public static void main(String[] args) {
+    // Log-olás
+    logger.info("Az alkalmazás indítása...");
 
     // Parancssori opciók beolvasása...
     for (String arg : args) {
@@ -37,13 +43,16 @@ public class Main {
     try {
       Fajlkezelo.BeolvasFajlbol(mentesFajlNev);
     } catch (FileNotFoundException e) {
-      System.err.println("Mentett játékállás nem található. Új játék indítása...");
+//      System.err.println("Mentett játékállás nem található. Új játék indítása...");
+      logger.info("Mentett játékállás nem található. Új játék indítása...");
       Tabla tabla = new Tabla(tablaSorokSzama, tablaOszlopokSzama);
       Jatek.JatekInicializalas();
     } catch (IOException e) {
-      System.err.println("Hiba történt: " + e.getMessage());
+//      System.err.println("Hiba történt: " + e.getMessage());
+      logger.error("Hiba történt: " + e.getMessage());
     } finally {
-      System.err.println("Kilépek...");
+//      System.err.println("Kilépek...");
+      logger.info("Az alkalmazás bezárása...");
     }
 
     // Tábla generálása
