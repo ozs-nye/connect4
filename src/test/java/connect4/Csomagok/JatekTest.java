5d425440793432907c5cb22068aa30afc1cb7fcc
ozs-nye <olah.zsolt@student.nye.hu>
2024-12-02 19:36:57 +0100

    újabb tesztek

--- src/test/java/connect4/Csomagok/JatekTest.java
+++ src/test/java/connect4/Csomagok/JatekTest.java
@@ -23,7 +23,7 @@ class JatekTest {
     }
 
     @Test
-    void testNyertesEllenorzesHorizontal() {
+    void testNyertesEllenorzesFuggoleges() {
         Tabla.setTablaMatrix(0, 0, Tabla.EMBER);
         Tabla.setTablaMatrix(0, 1, Tabla.EMBER);
         Tabla.setTablaMatrix(0, 2, Tabla.EMBER);
@@ -34,5 +34,32 @@ class JatekTest {
     @Test
     void testNyertesEllenorzesNoWin() {
         assertFalse(Jatek.NyertesEllenorzes(Tabla.EMBER));
+    }
+
+    @Test
+    void testDontetlenTelitettTabla() {
+        // Töltsük fel a táblát a döntetlen állás szerint
+        Tabla.setTablaMatrix(0, 0, Tabla.EMBER);
+        Tabla.setTablaMatrix(0, 1, Tabla.EMBER);
+        Tabla.setTablaMatrix(0, 2, Tabla.GEP);
+        Tabla.setTablaMatrix(0, 3, Tabla.GEP);
+
+        Tabla.setTablaMatrix(1, 0, Tabla.GEP);
+        Tabla.setTablaMatrix(1, 1, Tabla.GEP);
+        Tabla.setTablaMatrix(1, 2, Tabla.EMBER);
+        Tabla.setTablaMatrix(1, 3, Tabla.EMBER);
+
+        Tabla.setTablaMatrix(2, 0, Tabla.EMBER);
+        Tabla.setTablaMatrix(2, 1, Tabla.EMBER);
+        Tabla.setTablaMatrix(2, 2, Tabla.GEP);
+        Tabla.setTablaMatrix(2, 3, Tabla.GEP);
+
+        Tabla.setTablaMatrix(3, 0, Tabla.EMBER);
+        Tabla.setTablaMatrix(3, 1, Tabla.GEP);
+        Tabla.setTablaMatrix(3, 2, Tabla.GEP);
+        Tabla.setTablaMatrix(3, 3, Tabla.EMBER);
+
+        // Ellenőrizzük, hogy a rendszer döntetlent hirdet-e
+        assertTrue(Jatek.dontetlenEllenorzes(), "A tábla teljesen tele van, döntetlent kellett volna hirdetni.");
     }
 }
